4fe22a26ce6e3f89ac70b5aa6d76dc816a1d2ab3
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2022-05-27 20:26:49 -0500

    Update deployment scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,8 @@
-const { deployProxy, upgradeProxy } = require("@openzeppelin/truffle-upgrades");
+const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 
 const HomeOwnershipToken = artifacts.require("HomeOwnershipToken");
 
 module.exports = async function (deployer) {
-  const upgrade = false;
-  if (upgrade) {
-    var instance = await HomeOwnershipToken.deployed();
-    await upgradeProxy(instance.address, HomeOwnershipToken, {
-      deployer,
-    });
-  } else {
-    await deployProxy(HomeOwnershipToken, [], { deployer });
-    deployer.deploy(HomeOwnershipToken);
-  }
+  await deployProxy(HomeOwnershipToken, [], { deployer });
+  deployer.deploy(HomeOwnershipToken);
 };
