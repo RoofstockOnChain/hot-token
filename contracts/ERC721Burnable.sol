97e4b41fa520173a73682c760cc0eed24713cdbe
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2022-06-03 12:02:12 -0500

    Make ERC721Burnable abstract

--- contracts/ERC721Burnable.sol
+++ contracts/ERC721Burnable.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract ERC721Burnable is Initializable, ERC721Upgradeable, AccessControlUpgradeable {
+abstract contract ERC721Burnable is Initializable, ERC721Upgradeable, AccessControlUpgradeable {
     bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
     function __Burnable_init() internal onlyInitializing {
