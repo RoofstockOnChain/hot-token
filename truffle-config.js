e39aa697e8a401912102bd1ca07b63ca2e72fb06
Zachary McKinnon <zachary.wayne.mckinnon@gmail.com>
2022-07-05 09:06:28 -0500

    Add mainnet environment and verify function

--- truffle-config.js
+++ truffle-config.js
@@ -22,6 +22,15 @@ module.exports = {
       },
       network_id: 4,
     },
+    mainnet: {
+      provider: function () {
+        return new HDWalletProvider(
+          `${process.env.MNEMONIC}`,
+          `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`
+        );
+      },
+      network_id: 1,
+    }
   },
   plugins: ["truffle-plugin-verify"],
   api_keys: {
